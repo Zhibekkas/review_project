b9290dc8f4551a17510693c042ba49f61696a512
zhibek <zhibek.ksmva@gmail.com>
2022-03-05 12:43:48 +0600

    Product and Review models were created and added to admin.py. Migrations were made

--- source/webapp/admin.py
+++ source/webapp/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from webapp.models import Product, Review
 
 # Register your models here.
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'category', 'description', 'product_pic']
+    list_filter = ['name']
+    search_fields = ['name', 'category']
+    fields = ['name', 'category', 'description', 'product_pic']
+
+
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ['id', 'author', 'product', 'review_text', 'rating', 'moderation_status', 'creation_date', 'update_date']
+    list_filter = ['author', 'product']
+    search_fields = ['author']
+    fields = ['author', 'product', 'review_text', 'rating', 'moderation_status', 'creation_date', 'update_date']
+    readonly_fields = ['creation_date', 'update_date']
+
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Review, ReviewAdmin)
--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,3 +1,46 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 # Create your models here.
+User = get_user_model()
+DEFAULT_CATEGORY = 'other'
+CATEGORY_CHOICES = (
+    (DEFAULT_CATEGORY, 'Other'),
+    ('good', 'Goods'),
+    ('service', 'Services'),
+    ('pharma', 'Medicines'),
+    ('food', 'Food'),
+)
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100, blank=False, null= False, verbose_name='Product')
+    category = models.CharField(max_length=30, blank=False, null=False,default=DEFAULT_CATEGORY, choices=CATEGORY_CHOICES,
+                                verbose_name='Category')
+    description = models.TextField(max_length=2000, null=True, blank=True, verbose_name='Description')
+    product_pic = models.ImageField(null=True, blank=True, upload_to="product_pics", verbose_name="Picture")
+
+    def __str__(self):
+        return f'{self.name} - {self.category}'
+
+    class Meta:
+        verbose_name = 'Product'
+        verbose_name_plural = 'Products'
+
+
+class Review(models.Model):
+    author = models.ForeignKey(User, related_name='reviews', on_delete=models.CASCADE, verbose_name='Author')
+    product = models.ForeignKey('webapp.Product', on_delete=models.CASCADE, verbose_name='Product', related_name='reviews')
+    review_text = models.TextField(null=False, blank=False, verbose_name='Review')
+    rating = models.PositiveBigIntegerField(null=False, blank=False, validators=(MinValueValidator(1), MaxValueValidator(5)))
+    moderation_status = models.BooleanField(default=False, verbose_name='Moderation Status')
+    creation_date = models.DateTimeField(auto_now_add=True, verbose_name="Created at")
+    update_date = models.DateTimeField(auto_now_add=True, verbose_name="Updated at")
+
+    def __str__(self):
+        return f'{self.author} - {self.product}'
+
+    class Meta:
+        verbose_name = 'Review'
+        verbose_name_plural = 'Reviews'
